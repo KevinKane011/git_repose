8f3c54b58a325e9f492f4f0a8302e1530c1f4e89
Kevin Kane <kkane@kkane-mac.local>
2017-12-14 14:30:38 -0800

    ADD display of error to orbbec_frame.m

--- orbbec_frame.m
+++ orbbec_frame.m
@@ -10,6 +10,7 @@ if isa(raw,'char')
     frame = transpose(fread(fopen(raw), [640, 480], 'uint16=>uint16'));
     imshow(frame);
 else
+    disp('Error: input value was not a string');
     error('Error: input value was not a string');
 end
 end
